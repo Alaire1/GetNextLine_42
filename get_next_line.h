967bdfe146abbc2d09e9789817d97daf6c2961a2
Anita Karabanowicz <akaraban@c2s13d69.42wolfsburg.de>
2023-02-10 00:22:30 +0100

    dnwjndw

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akaraban <akaraban@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 22:31:34 by akaraban          #+#    #+#             */
+/*   Updated: 2023/02/10 00:06:05 by akaraban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stddef.h>
+
+char	*new_line(int fd, char *static_buffer);
+char	*fixed_line(char *static_buffer);
+char	*next_line(char *static_buffer);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s1);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
+
